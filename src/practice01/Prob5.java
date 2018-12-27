d5521c8b1aacc758f1d74431f0a5600edfa68b88
jooyoung <jooyoung@DESKTOP-KUP8SIT>
2018-12-27 18:45:50 +0900

    practice01 commit

--- src/practice01/Prob5.java
+++ src/practice01/Prob5.java
@@ -4,6 +4,21 @@ public class Prob5 {
 
 	public static void main(String[] args) {
 		for(int i=1; i<100; i++) {
+			int cnt = 0;
+			for(int j=0; j<String.valueOf(i).length(); j++) {
+				if(String.valueOf(i).substring(j,j+1).equals("3")
+					|| String.valueOf(i).substring(j,j+1).equals("6")
+					|| String.valueOf(i).substring(j,j+1).equals("9")) {
+					cnt ++;
+				}
+			}
+			if(cnt > 0) {
+				System.out.print(i+" ");
+				for(int k=0; k<cnt; k++) {
+					System.out.print("짝");
+				}
+				System.out.println();
+			}
 		}
 	}
 }
